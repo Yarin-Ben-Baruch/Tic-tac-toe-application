a124b3a7feea2e94cb2f9b9cb5ac042600b089b3
yarinbenbaruch <yarinbenbaruch97@gmail.com>
2021-12-01 13:43:12 +0200

    add all

---
+++ src/Player.java
@@ -0,0 +1,33 @@
+
+public abstract class Player {
+
+    private String name;
+    private eMark defaultSign;
+
+    //                      name            X || O
+    public Player(String i_name, eMark i_DefaultSign) {
+        setName(i_name);
+        setDefaultSign(i_DefaultSign);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public eMark getDefaultSign() {
+        return defaultSign;
+    }
+
+    public void setDefaultSign(eMark defaultSign) {
+        this.defaultSign = defaultSign;
+    }
+
+    abstract void playTurn(Board board, eMark defaultSign, int row, int col);
+
+    abstract int getCoordinates();
+
+}
