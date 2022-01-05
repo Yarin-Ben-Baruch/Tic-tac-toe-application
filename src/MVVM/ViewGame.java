15de3645fd394e3aabba24d1fa61e7ba241292a3
matan <matanbare25@gmail.com>
2022-01-05 12:31:05 +0200

    adding First Screen menu

--- src/MVVM/ViewGame.java
+++ src/MVVM/ViewGame.java
@@ -9,16 +9,21 @@ import Players.HumanPlayer;
 import Players.WhateverPlayer;
 import Renderers.VoidRenderer;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.IOException;
 
 public class ViewGame extends JFrame {
 
-    private JFrame startFrame = new JFrame();
-    private JPanel titlePanel = new JPanel();
-    private JTextField titletextfield =new JTextField();
-    private JButton[] optionsButtons = new JButton[4];
+    private JFrame startFrame = new JFrame("Game options");
+    private JPanel playersPanel = new JPanel();
+    private JPanel boardSizePanel = new JPanel();
+    private JButton[] boardSizeSelectionButtons = new JButton[3];
+    private JButton[] participantsSelectionButtons = new JButton[2];
+
     private JPanel buttonPanel = new JPanel();
     private Tournament tournament;
 
@@ -28,53 +33,47 @@ public class ViewGame extends JFrame {
 
     public void setStartFrame() {
 
-        startFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        startFrame.setSize(500, 500);
-        startFrame.setLayout(new BorderLayout());
+        startFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        startFrame.setSize(500, 250);
+        startFrame.setResizable(true);
+        startFrame.setLocationRelativeTo(null);
+        try {
+            startFrame.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("src/images/backGroundForMenu.jpg")))));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        startFrame.setLayout(null);
 
-        titlePanel.setBackground(Color.pink);
+        String[] titleForButtons = {"Player vs Player", "Player vs CPU"};
+        int x = 50;
+        for (int i = 0; i < 2; i++) {
 
-        titletextfield.setBackground(new Color(25,25,25));
-        titletextfield.setForeground( Color.pink);
-        titletextfield.setFont(new Font("Ink Free",Font.BOLD,70));
-        titletextfield.setHorizontalAlignment(JLabel.CENTER);
-        titletextfield.setText("Tic-Tac-Toe");
-        titletextfield.setOpaque(true);
-
-        titlePanel.setLayout(new BorderLayout());
-        titlePanel.setBounds(0,0,800,100);
-
-        buttonPanel.setLayout(new GridLayout(3,1));
-        buttonPanel.setBackground(new Color(150,150,150));
-
-
-        String[] titleForButtons = {"Easy", "Hard", "Against myself"};
-        String playerType1 = "human", playerType2, selection, rendererType, numberOfRoundsStr = null;
-
-        for(int i=0;i<3;i++) {
-            optionsButtons[i] = new JButton(titleForButtons[i]);
-            buttonPanel.add(optionsButtons[i]);
-            optionsButtons[i].setFont(new Font("MV Boli",Font.BOLD,36));
-            optionsButtons[i].setFocusable(false);
-
+            participantsSelectionButtons[i] = new JButton();
+            participantsSelectionButtons[i].setFocusable(false);
+            participantsSelectionButtons[i].setText(titleForButtons[i]);
+            participantsSelectionButtons[i].setBackground(new Color(0xFFFFFF));
+            participantsSelectionButtons[i].setBounds(x, 30, 135, 50);
+            x += 145;
+            startFrame.add(participantsSelectionButtons[i]);
         }
 
-        optionsButtons[0].addActionListener(e -> startPlayLevelEasy());
-        optionsButtons[1].addActionListener(e -> startPlayLevelExpert());
-        optionsButtons[2].addActionListener(e -> startPlayAgainstMyself());
+        x = 50;
+        String[] levels = {"Easy", "Hard"};
+        JComboBox levelComboBox = new JComboBox(levels);
+        levelComboBox.setBounds(x, 100, 135, 50);
+        startFrame.add(levelComboBox);
 
-        titlePanel.add(titletextfield);
-        startFrame.add(titlePanel,BorderLayout.NORTH);
-        startFrame.add(buttonPanel);
+        String[] boardSize = {"3x3 Board", "4x4 Board", "5x5 Board"};
+        x += 145;
+        JComboBox boardComboBox = new JComboBox(boardSize);
+        boardComboBox.setBounds(x, 100, 135, 50);
+        startFrame.add(boardComboBox);
         startFrame.setVisible(true);
-
-
     }
 
     public void actionPerformed(ActionEvent e) {
 
     }
-
 
     public void startPlayLevelEasy() {
         //Object eMark = null; ****************************************************************************
@@ -100,6 +99,5 @@ public class ViewGame extends JFrame {
     public static void main(String[] args) {
         ViewGame viewGame = new ViewGame();
     }
-
-
 }
+
