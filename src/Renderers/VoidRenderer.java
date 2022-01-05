4683dd20069297fabbe21936a556e7187a1a94da
Daniel-Gelfand <76646067+Daniel-Gelfand@users.noreply.github.com>
2022-01-05 12:58:23 +0200

    Update Comments on VoidRenderer Class.

--- src/Renderers/VoidRenderer.java
+++ src/Renderers/VoidRenderer.java
@@ -5,6 +5,10 @@ import Boards.Board;
 
 public class VoidRenderer implements iRenderer{
 
+    /**
+     * 'renderBoard' is method that we use for empty Board.
+     * @param board
+     */
     public void renderBoard(Board board) {
 
     }
