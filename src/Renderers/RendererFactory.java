f23271f2b5fc1da0fd25807907c98e38dbc21e3e
Daniel-Gelfand <76646067+Daniel-Gelfand@users.noreply.github.com>
2022-01-05 12:57:12 +0200

    Update Comments on RendererFactory Class.

--- src/Renderers/RendererFactory.java
+++ src/Renderers/RendererFactory.java
@@ -3,6 +3,11 @@ package Renderers;
 
 public class RendererFactory {
 
+    /**
+     * 'buildRenderer' is method that accept the user type of UI.
+     * @param i_Type
+     * @return
+     */
     public iRenderer buildRenderer(String i_Type) {
 
         switch(i_Type) {
