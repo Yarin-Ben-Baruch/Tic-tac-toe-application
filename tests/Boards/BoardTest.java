93bf3d992a656d485122f0d599ef1e16aba9a109
Daniel-Gelfand <76646067+Daniel-Gelfand@users.noreply.github.com>
2022-01-05 11:43:12 +0200

    done board tests.

---
+++ tests/Boards/BoardTest.java
@@ -0,0 +1,52 @@
+package Boards;
+
+import Enums.eGameStatus;
+import Enums.eMark;
+import static org.junit.Assert.*;
+
+class BoardTest {
+
+    private Board board;
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        board = new Board();
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+        board = null;
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBoard() {
+        assertEquals(board.getBoard(),board.getBoard());
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void putMark() {
+        boolean putMarkX = board.putMark(eMark.X,3,3);
+        boolean putMarkO = board.putMark(eMark.O,3,2);
+        assertEquals(putMarkX,putMarkO);
+    }
+
+    @org.junit.jupiter.api.Test
+    void gameStatus() {
+        eGameStatus xWin = eGameStatus.X_WIN;
+        assertEquals(xWin, eGameStatus.X_WIN );
+        
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMark() {
+        eMark markOnBoard = board.getMark(0,0);
+        assertEquals(eMark.BLANK,markOnBoard);
+    }
+
+    @org.junit.jupiter.api.Test
+    void setHaveWinner() {
+        boolean haveWinner = board.isHaveWinner();
+        assertEquals(haveWinner,false);
+    }
+}
